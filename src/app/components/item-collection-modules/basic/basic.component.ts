865eb22aab8596c5946c584dbd5536eeefe94537
Baeyung <ahmadsheikh617@gmail.com>
2024-05-12 03:03:31 +0500

    moving things around

--- src/app/components/item-collection-modules/basic/basic.component.ts
+++ src/app/components/item-collection-modules/basic/basic.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CarouselModel } from '../../shared/carousel/carousel.component';
 
 @Component({
     selector: 'app-basic',
@@ -6,7 +7,88 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./basic.component.scss']
 })
 export class BasicComponent implements OnInit {
-    constructor() {}
+    items: CarouselModel[] = [];
+    constructor(
+    ) {}
 
-    ngOnInit() {}
+    ngOnInit(): void {
+        this.items = [
+            {
+                id: 1,
+                title: 'Karandi',
+                price: 5000,
+                children: [
+                    {
+                        templateContent: './../../../assets/suits/1.jpg'
+                    },
+                    {
+                        templateContent: './../../../assets/suits/2.jpg'
+                    },
+                    {
+                        templateContent: './../../../assets/suits/3.jpg'
+                    },
+                    {
+                        templateContent: './../../../assets/suits/4.jpg'
+                    },
+                    {
+                        templateContent: './../../../assets/suits/5.jpg'
+                    },
+                    {
+                        templateContent: './../../../assets/suits/6.jpg'
+                    },
+                    {
+                        templateContent: './../../../assets/suits/7.jpg'
+                    },
+                    {
+                        templateContent: './../../../assets/suits/8.jpg'
+                    },
+                    {
+                        templateContent: './../../../assets/suits/9.jpg'
+                    },
+                    {
+                        templateContent: './../../../assets/suits/10.jpg'
+                    }
+                ]
+            },
+            {
+                id: 1,
+                price: 5000,
+                title: 'Cotton',
+                children: [
+                    {
+                        templateContent: './../../../assets/suits/3.jpg'
+                    },
+                    {
+                        templateContent: './../../../assets/suits/4.jpg'
+                    }
+                ]
+            },
+            {
+                id: 1,
+                price: 5000,
+                title: 'Silk',
+                children: [
+                    {
+                        templateContent: './../../../assets/suits/5.jpg'
+                    },
+                    {
+                        templateContent: './../../../assets/suits/6.jpg'
+                    }
+                ]
+            },
+            {
+                id: 1,
+                price: 5000,
+                title: 'Boski',
+                children: [
+                    {
+                        templateContent: './../../../assets/suits/7.jpg'
+                    },
+                    {
+                        templateContent: './../../../assets/suits/8.jpg'
+                    }
+                ]
+            }
+        ];
+    }
 }
