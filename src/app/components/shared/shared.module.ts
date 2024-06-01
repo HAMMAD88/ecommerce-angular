77271abd3530a7cae64827e3bbd893e948d4cd87
Baeyung <ahmadsheikh617@gmail.com>
2024-06-02 02:38:04 +0500

    separating header component

--- src/app/components/shared/shared.module.ts
+++ src/app/components/shared/shared.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { ButtonModule } from 'primeng/button';
 import { CarouselModule } from 'primeng/carousel';
 import { CarouselComponent } from './carousel/carousel.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
     imports: [CommonModule, ButtonModule, CarouselModule],
-    declarations: [CarouselComponent],
-    exports: [CarouselComponent]
+    declarations: [CarouselComponent, HeaderComponent],
+    exports: [CarouselComponent, HeaderComponent]
 })
 export class SharedModule {}
